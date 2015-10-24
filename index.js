e9619c0b90dd3f3607e144fe00f12e98facaa91e
Stephen Whitmore <stephen.whitmore@gmail.com>
2015-10-24 08:58:27 -0700

    configstore for storage; prompt for prompting API keys

--- index.js
+++ index.js
@@ -1,18 +1,51 @@
-var secrets = require('./secrets')
-
-var twitterRss = require('rss-twitter')(secrets.CONSUMER_KEY, secrets.CONSUMER_SECRET, secrets.ACCESS_TOKEN, secrets.ACCESS_SECRET);
-
 var http = require('http')
+var prompt = require('prompt')
+var rssTwitter = require('rss-twitter')
+var configstore = require('configstore')
 
-http.createServer(function (req, res) {
-  var user = req.url.substring(1)
+var conf = new configstore('twitter-rss-server')
 
-  twitterRss.feed(user, function (err, feed) {
+promptForKeys(startServer)
+
+function startServer () {
+  var twitterApi = rssTwitter(conf.get('CONSUMER_KEY'), conf.get('CONSUMER_SECRET'), conf.get('ACCESS_TOKEN'), conf.get('ACCESS_SECRET'))
+
+  http.createServer(function (req, res) {
+    var user = req.url.substring(1)
+
+    twitterApi.feed(user, function (err, feed) {
+      if (err) {
+        res.end(err)
+      } else {
+        res.end(feed.render('rss-2.0'))
+      }
+    })
+  }).listen(6000)
+}
+
+function promptForKeys (cb) {
+  var fields = [
+    'CONSUMER_KEY',
+    'CONSUMER_SECRET',
+    'ACCESS_TOKEN',
+    'ACCESS_SECRET',
+  ]
+  var missingFields = fields.filter(function (f) { return !conf.get(f) })
+  if (!missingFields.length) {
+    cb()
+    return
+  }
+
+  prompt.start()
+
+  prompt.get(missingFields, function (err, res) {
     if (err) {
-      res.end(err)
+      return process.exit(err)
     } else {
-      res.end(feed.render('rss-2.0'));
+      Object.keys(res).forEach(function (k) {
+        conf.set(k, res[k])
+      })
+      cb()
     }
   })
-}).listen(6000)
-
+}
